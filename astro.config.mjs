ee9ea0fa05d9903ab0f2a4f5c2bbf6621433ad00
Adhiraj Bhatia <adhirajb1109@gmail.com>
2023-05-27 07:15:01 +0530

    fix: Replaced Astro.url from Astro.site to obtain production url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://adhiraj.vercel.app/',
   integrations: [tailwind(), react(), mdx()],
   markdown: {
     shikiConfig: {
