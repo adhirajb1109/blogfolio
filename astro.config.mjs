a3af4d30748fb60dff8c0c68b1a7511977cc7bda
Adhiraj Bhatia <adhirajb1109@gmail.com>
2023-06-13 22:51:01 +0530

    fix: Fixed a Bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://adhiraj.vercel.app/",
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind()],
   markdown: {
     shikiConfig: {
       theme: "nord",
