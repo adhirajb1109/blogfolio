2c2d4dad1431ec50b4cb218de1dec08efd1737cc
Adhiraj Bhatia <adhirajb1109@gmail.com>
2023-07-23 18:18:12 +0530

    feat: Made some design changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,29 +10,6 @@ module.exports = {
         xxs: "375px",
       },
     },
-    extend: {
-      colors: {
-        turquoise: {
-          50: "#effdfd",
-          100: "#d2f7f9",
-          200: "#aaf0f4",
-          300: "#76e4ec",
-          400: "#39d0dd",
-          500: "#14b4c6",
-          600: "#0892a2",
-          700: "#097684",
-          800: "#0b5e6b",
-          900: "#0d4f5a",
-        },
-      },
-      spacing: {
-        128: "32rem",
-        144: "36rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
   },
   plugins: [
     require("@tailwindcss/typography")
